3a7fa99eb1df5c50a0df255aa2a176e6a86a9f75
Gustavo Schip <gustavoschip@proton.me>
2023-07-19 23:46:22 +0200

    Init

--- ninjakiwi_api/API/BTDB2/function.py
+++ ninjakiwi_api/API/BTDB2/function.py
@@ -1,5 +1,46 @@
 """API-level package for NinjaKiwi API."""
 
+import re
+from enum import Enum
+from typing import Optional
 
-async def btdb2_url_factory(data: str) -> str | None:
-    pass
+__endpoint__ = "https://data.ninjakiwi.com/"
+__game__ = "battles2"
+
+
+class ChallengeFilter(str, Enum):
+    NEWEST = "newest"
+    TRENDING = "trending"
+    DAILY = "daily"
+
+
+async def btdb2_url_factory(data: str, **options) -> Optional[str]:
+    types = [
+        "homs",
+        "users",
+    ]
+
+    if data not in types:
+        return None
+
+    base_url = f"{__endpoint__}{__game__}/{data}"
+
+    if data == "homs":
+        homID = options.get("homID")
+        if homID is not None:
+            homID_pattern = re.compile(r"^[A-Za-z0-9_-]+$")
+            if not homID_pattern.match(homID):
+                return None
+            return f"{base_url}/{homID}/leaderboard"
+        else:
+            return base_url
+
+    if data == "users":
+        userID = options.get("userID")
+        userID_pattern = re.compile(r"^[A-Za-z0-9_-]+$")
+        if userID is not None and userID_pattern.match(userID):
+            return f"{base_url}/{userID}"
+
+        return None
+
+    return None
--- tests/test_main.py
+++ tests/test_main.py
@@ -6,10 +6,20 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_fetch():
+async def test_fetch_btd6():
     """Sample pytest test function."""
     from ninjakiwi_api import fetch
 
     results = await fetch("BTD6", "races")
+
+    assert "success" in results
+
+
+@pytest.mark.asyncio
+async def test_fetch_btdb2():
+    """Sample pytest test function."""
+    from ninjakiwi_api import fetch
+
+    results = await fetch("BTDB2", "homs")
 
     assert "success" in results
